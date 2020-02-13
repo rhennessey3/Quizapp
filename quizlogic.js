0567ba6b41aa57235b57e788809c6bb596daf420
Richard Hennessey III <Rick@Richards-iMac.local>
2020-02-13 14:17:29 -0500

    script changes

--- quizlogic.js
+++ quizlogic.js
@@ -1,33 +1,7 @@
-
-
 $(document).ready()
 
 // q's followed by choices followed by an array of values that are aligned with the choices.
 // I want to be able to "weight the value of the choices". this should be added to the end score.
-
-var quiz = [
-    {
-        "question": "How old are you?",
-        "choices": ["18-25", "26-34", "35-46", "46-55", "55+"],
-        "answer": [20, 18, 16, 10, 5]
-    }, {
-        "question": "Where do you live?",
-        "choices": ["Large City", "Medium City", "Large Town", "Small Town", "Rural Area"],
-        "answer": [4, 8, 12, 16, 20]
-    }, {
-        "question": "How would you rate your physical condition?",
-        "choices": ["Ultra runner", "Marathon runner", "I like to run", "Whats running?"],
-        "answer": [20, 16, 12, 8, 4]
-    }, {
-        "question": "Rate your food preperation skills",
-        "choices": ["I can cook with a campfire", "I can hunt", "I feel at home in the kitchen", "I mostly microwave meals", "Instant noodles"],
-        "answer": [20, 16, 12, 8, 4]
-    }, {
-        "question": "How well can you navigate?",
-        "choices": ["I can get home blindfolded", "Capable with a compass", "With landmarks", "Left or right you pick", "I pick a direction and run"],
-        "answer": [20, 16, 12, 8, 4]
-    }
-];
 
 
 $('.submit').click(event => {
